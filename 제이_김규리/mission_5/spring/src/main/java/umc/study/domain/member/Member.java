78e9d9e6f3ecad337a47860b37ca46fe4f0951ea
OrangeKim04 <queemkimu@naver.com>
2025-05-02 05:06:35 +0900

    5주차 미션

---
+++ 제이_김규리/mission_5/spring/src/main/java/umc/study/domain/member/Member.java
@@ -0,0 +1,23 @@
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private String specAddress;
+
+    private LocalDate inactiveDate;
+
+    private String email;
+
+    private Integer point;
+}
