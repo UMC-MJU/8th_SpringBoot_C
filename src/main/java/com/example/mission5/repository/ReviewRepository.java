f603c73b036c6bd01824457170cbea5a2fa33d25
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-3.local>
2025-05-03 23:36:30 +0900

    feat: git 수정

--- src/main/java/com/example/mission5/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mission5.repository;
-
-import com.example.demo.domain.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ReviewRepository extends JpaRepository<Review, UUID> {
-}
