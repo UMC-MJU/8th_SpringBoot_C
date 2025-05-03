2ae8f03410bfaac170ff095e6bf9b4fae65c8abb
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-3.local>
2025-05-03 23:29:56 +0900

    refactor: Spring Boot 프로젝트 디렉토리 구조 정리

---
+++ 빽_백가현/src/main/java/com/example/mission5/domain/Review.java
@@ -0,0 +1,27 @@
+package com.example.mission5.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "review")
+public class Review extends BaseEntity{
+    String content;
+    float score;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+}
---
+++ 빽_백가현/src/main/java/com/example/mission5/repository/MemberPreferRepository.java
@@ -0,0 +1,12 @@
+package com.example.mission5.repository;
+
+import com.example.demo.domain.MemberPrefer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+import java.util.UUID;
+
+public interface MemberPreferRepository extends JpaRepository<MemberPrefer, UUID> {
+    @Modifying
+    void deleteByMemberId(UUID memberId);
+}
