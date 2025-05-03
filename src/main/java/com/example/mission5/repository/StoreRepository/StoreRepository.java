cde73ed2ff3b5c70c01666e30ade8ca7167b2afa
백가현 <127074448+GahBaek@users.noreply.github.com>
2025-05-03 23:33:04 +0900

    Delete src directory

--- src/main/java/com/example/mission5/repository/StoreRepository/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mission5.repository.StoreRepository;
-
-import com.example.demo.domain.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface StoreRepository extends JpaRepository<Store, UUID>, StoreRepositoryCustom {
-}
