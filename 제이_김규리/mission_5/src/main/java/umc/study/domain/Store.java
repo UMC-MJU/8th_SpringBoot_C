bd95413ae8a37b69541f68bcebf3ca9c6ee4e0eb
OrangeKim04 <queemkimu@naver.com>
2025-05-12 01:00:34 +0900

    feat: 실습 코드

--- 제이_김규리/mission_5/src/main/java/umc/study/domain/Store.java
+++ 제이_김규리/mission_5/src/main/java/umc/study/domain/Store.java
@@ -1,8 +1,11 @@
 package umc.study.domain;
 
-import jakarta.persistence.*;
 import lombok.*;
 import umc.study.domain.base.BaseEntity;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -15,15 +18,32 @@ public class Store extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+
+    private String address;
+
+    private Float score;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "region_id")
     private Region region;
 
-    @Column(nullable = false, length = 50)
-    private String name;
+    @Builder.Default
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Mission> missionList = new ArrayList<>();
 
-    @Column(nullable = false, length = 50)
-    private String address;
+    @Builder.Default
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
 
-    private Float score;
+    @Override
+    public String toString() {
+        return "Store{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", score=" + score +
+                ", region=" + (region != null ? region.getName() : "N/A") + // region의 이름 출력
+                '}';
+    }
 }
